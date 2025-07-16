853be6111cc8ac29dba6e7cc8c85ade45045b7b7
skoochakpour <skoochakpour@myseneca.ca>
2025-07-16 14:30:29 -0400

    Add network Security Group Variables

--- variables.tf
+++ variables.tf
@@ -31,5 +31,17 @@ variable "nsg_name" {
   type        = string
   description = "Name of the Network Security Group"
 }
-
-
+ variable "security_rule" {
+  type = list(object({
+    name                       = string
+    priority                   = number
+    direction                  = string
+    access                     = string
+    protocol                   = string
+    source_port_range          = string
+    destination_port_range     = string
+    source_address_prefix      = string
+    destination_address_prefix = string
+  }))
+  description = "List of security rules for the Network Security Group"
+ }
